9035e0079671e12ea2dc12bcdb128efb7ed815c1
Haeryz <haeriz42069@gmail.com>
2024-10-02 09:10:57 +0700

    Added firebase and auth view

---
+++ skibidiskibidisigma/lib/app/modules/authentication/controllers/authentication_controller.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+
+class AuthenticationController extends GetxController {
+  //TODO: Implement HomeController
+
+  final count = 0.obs;
+
+
+
+  void increment() => count.value++;
+}
--- skibidiskibidisigma/lib/app/modules/splash_screen/views/splash_view.dart
+++ skibidiskibidisigma/lib/app/modules/splash_screen/views/splash_view.dart
@@ -1,6 +1,7 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:skibidiskibidisigma/app/modules/authentication/views/authentication_view.dart';
 import '../../home/views/home_view.dart'; 
 
 class SplashScreenView extends StatelessWidget {
@@ -11,7 +12,7 @@ class SplashScreenView extends StatelessWidget {
     return AnimatedSplashScreen(
       duration: 3000, 
       splash: Image.asset('assets/icon/splash.png'), 
-      nextScreen: const HomeView(), 
+      nextScreen: const AuthenticationView(), 
       splashTransition: SplashTransition.scaleTransition,
       backgroundColor: Colors.blue,
     );
