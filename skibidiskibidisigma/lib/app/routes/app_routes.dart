164a34f1a55f75b48432bd395c809427754b0c36
Haeryz <haeriz42069@gmail.com>
2024-10-02 18:35:25 +0700

    Added profileview in sign in

--- skibidiskibidisigma/lib/app/routes/app_routes.dart
+++ skibidiskibidisigma/lib/app/routes/app_routes.dart
@@ -4,10 +4,14 @@ abstract class Routes {
   Routes._();
   static const SPLASH = _Paths.SPLASH; // Add SPLASH route
   static const HOME = _Paths.HOME;
+  static const AUTHENTICATION = _Paths.AUTHENTICATION;
+  static const PROFILE = _Paths.PROFILE;
 }
 
 abstract class _Paths {
   _Paths._();
   static const SPLASH = '/splash';
   static const HOME = '/home';
+  static const AUTHENTICATION = '/authentication';
+  static const PROFILE = '/profile';
 }
