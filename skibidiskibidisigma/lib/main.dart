3b317780f4fcfe4c9b07659227a23d16bfa79f4b
Mikasenz <riyo.t.adinata@gmail.com>
2024-10-03 12:33:23 +0700

    Menambah HomePage

--- skibidiskibidisigma/lib/main.dart
+++ skibidiskibidisigma/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_core/firebase_core.dart';
+
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -6,8 +6,7 @@ import 'package:get/get.dart';
 import 'app/routes/app_pages.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+
   runApp(
     GetMaterialApp(
       title: "Application",
