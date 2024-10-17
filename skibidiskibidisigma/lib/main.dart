a52b202aec8aad020cc78e57383f201bcbbaaf78
TwicePie <kknurarief@gmail.com>
2024-10-17 10:38:47 +0700

    Link home into Wikipedia

--- skibidiskibidisigma/lib/main.dart
+++ skibidiskibidisigma/lib/main.dart
@@ -9,7 +9,6 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();  
   await Firebase.initializeApp();  
   Get.lazyPut<local.SearchController>(() => local.SearchController());
-  await dotenv.load();
 
   runApp(
     GetMaterialApp(
